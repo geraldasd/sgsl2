f5227f8e00b42db8097630260ee95561b3dc42ac
geraldasd <g-tecmadtic@hotmail.com>
2016-07-07 08:57:38 +0800

    Add files via upload

---
+++ app/pages/levels/levels.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {TwinklePage} from './beginner/twinkle';
+
+@Component({
+  templateUrl: 'build/pages/levels/levels.html'
+})
+export class Levels {
+  constructor(private nav: NavController) {
+
+  }
+  levels: string="beginner";
+
+  openPage(event){
+    this.nav.push(TwinklePage);
+  };
+}
